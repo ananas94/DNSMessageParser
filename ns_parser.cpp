212ec86746a0b08210700115809320279e3b1642
ananas <den.ananas94@gmail.com>
2021-08-23 13:23:03 +0300

    comment

--- ns_parser.cpp
+++ ns_parser.cpp
@@ -786,7 +786,7 @@ int main()
 
         dns_message_t dm = mp.GetDnsMessage();
         std::cout << dm << std::endl;
-    } catch (std::invalid_argument& e) {
+    } catch (std::invalid_argument& e) {  //exceptions could be not optimal soulution, if broken message is a rule, not an exception 
         std::cout << "could not parse input message" << e.what();
         throw;
     }
