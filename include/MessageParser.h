dff706f5817bac8aef877baf48af9a94136e2990
ananas94 <den.anans94@gmail.com>
2021-09-14 00:37:23 +0300

    clang-format

--- include/MessageParser.h
+++ include/MessageParser.h
@@ -2,11 +2,11 @@
 #define MESSAGE_PASER
 #include "dns_structures.h"
 
+#include <cstring>
 #include <memory>
 #include <vector>
-#include <cstring>
 
-uint16_t ntoh(uint16_t net); 
+uint16_t ntoh(uint16_t net);
 uint32_t ntoh(uint32_t net);
 
 class MessageParser {
@@ -20,22 +20,20 @@ public:
   std::unique_ptr<RData> GetRData(uint16_t type);
   std::string GetDomainName(bool couldBeCompressed = true);
   std::vector<uint8_t> GetRawData(size_t length);
-  template <typename T> T Get()
-  {
-  T ret;
-  if (m_offset + sizeof(ret) > m_raw_data.size())
-    throw std::invalid_argument("out of bound");
-  std::memcpy(&ret, m_raw_data.data() + m_offset, sizeof(ret));
-  m_offset += sizeof(ret);
-  ret = ntoh(ret);
-  return ret;
-}
+  template <typename T> T Get() {
+    T ret;
+    if (m_offset + sizeof(ret) > m_raw_data.size())
+      throw std::invalid_argument("out of bound");
+    std::memcpy(&ret, m_raw_data.data() + m_offset, sizeof(ret));
+    m_offset += sizeof(ret);
+    ret = ntoh(ret);
+    return ret;
+  }
   size_t GetCurrentOffset() { return m_offset; };
 
 private:
   size_t m_offset;
   std::vector<uint8_t> m_raw_data;
 };
-
 
 #endif
